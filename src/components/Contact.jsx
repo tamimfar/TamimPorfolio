925bf1e1b877f310107c84edd94b74cb6ac8e759
tamimfar <t41296193@gmail.com>
2026-07-04 19:31:41 +0600

    Fix build issue1

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import emailIcon from '../assets/emailIcon.svg'
-import phoneicon from '../assets/phoneIcon.svg'
+import phoneicon from '../assets/phone.svg'
 import telegramIcon from '../assets/telegramIcon.svg'
 
 function Contact() {
